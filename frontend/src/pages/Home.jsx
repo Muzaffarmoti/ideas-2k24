8f9da4eefc816fe6ff8b736932eb72fe0842ed40
deekshas18 <deekshas182003@gmail.com>
2024-04-03 22:12:21 +0530

    pushing the header

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../components/Header'
 
 function Home() {
     return (
         <div>
+            <Header/>
             <h1>home</h1>
         </div>
     )
