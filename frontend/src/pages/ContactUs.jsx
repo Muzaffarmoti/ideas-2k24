d81f1c68308fccb328215715ca2c39e548d01afe
Muzaffarmoti <muzaffarmoti@gmail.com>
2024-04-04 21:41:45 +0530

    added background for contact page

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -1,43 +1,51 @@
 import React from 'react'
-import './ContactUs.css'
+import '../Css/ContactUs.css'
+import art_video from '../assets/videos/Art_at_contact.mp4'
 
 function ContactUs() {
     return (
         <div className='contact_main_container'>
-            <h3 className='contact_header'>Contact us </h3>
-           <div className='contact_container1' >
+              <h3 className='contact_header'>Contact us </h3>
+            <div className='contact_container1' >
             <div className="container_map" ><iframe width="600" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 
                src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Siddaganga%20Institute%20Of%20Technology%20Bangalore%20-%20Honnavar%20Hwy,%20Chandana%20Complex,%20Tumakuru,%20Karnataka%20572103+(SIT)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
                <a href="https://www.gps.ie/">gps vehicle tracker</a></iframe>
             </div>
-            <div className='cont'>
+            <div className='cont'>  
+            <div className='back'>
+           <video autoPlay loop muted>
+                <source src={art_video} type='video/mp4'/>
+            </video>
+            </div>  
+             <div className='content'>          
                 <div className='container_text1'>
                     <div className='text1'>
-                        <h6 >KCK</h6>
-                        chair,IEEE SIT SB<br/>
-                        📞+91 9999999999<br/>
-                        ✉️ kck@gamil.com
+                        <h6 >Kona Chandra Kiran</h6>
+                        Chair,IEEE SIT SB<br/>
+                        📞+91 99897 94662<br/>
+                        ✉️ konachandrakiran555@gmail.com
                     </div>
                     <div className='text1'>
-                        <h6 >Adarsh</h6>
+                        <h6 >Adarsh Reddy P</h6>
                         Vicechair,IEEE SIT SB<br/>
-                        📞+91 9999999999<br/>
-                        ✉️ adarsh@gamil.com
+                        📞+91 95914 88158<br/>
+                        ✉️ adarshpreddy@ieee.org
                     </div>             
                 </div>
                 <div className='container_text1'>
                    <div className='text1'>
-                        <h6 >Nanditha</h6>
-                        Vicechair,WIE IEEE SIT SB<br/>
-                        📞+91 9999999999<br/>
-                        ✉️ Nanditha@gamil.com
+                        <h6 >Chandana R</h6>
+                        Chair,WIE IEEE SIT SB<br/>
+                        📞+91 79757 20929<br/>
+                        ✉️ rchandana1711@gmail.com
                    </div>
                    <div className='text1'>
-                        <h6 >Chandana</h6>
-                        chair,WIE IEEE SIT SB<br/>
-                        📞+91 9999999999<br/>
-                        ✉️ Chandana@gamil.com
-                   </div>             
+                        <h6 >Nanditha B U</h6>
+                        Vicechair,WIE IEEE SIT SB<br/>
+                        📞+91 95916 19331<br/>
+                        ✉️ nandithabu53@gmail.com
+                    </div>
+                </div> 
                 </div>
             </div>
            </div> 
@@ -47,46 +55,46 @@ function ContactUs() {
                    <div className='text1'>
                         <h6 >Anagha N</h6>
                         Lead,Web Master IEEE SIT SB<br/>
-                        📞+91 9999999999<br/>
-                        ✉️ Anagha@gamil.com
+                        📞+91 78927 36753<br/>
+                        ✉️ anagha2003.13@gamil.com
                    </div>
                    <div className='text1'>
                         <h6 >Rishith P</h6>
                         Lead,Design Master IEEE SIT SB<br/>
-                        📞+91 9999999999<br/>
-                        ✉️ Rishith@gamil.com
+                        📞+91 78924 94498<br/>
+                        ✉️ rishithp2003@gmail.com
                   </div>
                    <div className='text1'>
-                        <h6 >Muzaffar M</h6>
+                        <h6 >Muzaffar M M</h6>
                         Joint Treasure, IEEE SIT SB<br/>
-                        📞+91 9999999999<br/>
-                        ✉️ Muzaffar@gamil.com
+                        📞+91 90368 99938<br/>
+                        ✉️ muzaffarmoti@gamil.com
                   </div>
            </div>
             <div className='container_text_sub1'>
                    <div className='text1'>
-                        <h6>Deeksha</h6>
+                        <h6>Deeksha S</h6>
                         Member, IEEE SIT SB<br/>
-                        📞+91 9999999999<br/>
-                        ✉️ Deeksha@gamil.com
+                        📞+91 63664 81557<br/>
+                        ✉️ deekshas182003@gmail.com
                    </div>
                    <div className='text1'>
-                        <h6 >Ananya Br</h6>
+                        <h6 >Ananya BR</h6>
                         Member, IEEE SIT SB<br/>
-                        📞+91 9999999999<br/>
-                        ✉️ Ananya@gamil.com
+                        📞+91 99110 449927 <br/>
+                        ✉️ ananyasetty127@gmail.com 
                   </div>
                    <div className='text1'>
-                        <h6 >Likith</h6>
+                        <h6 >Likith S H</h6>
                         Member, IEEE SIT SB<br/>
-                        📞+91 9999999999<br/>
-                        ✉️ Likith@gamil.com
+                        📞+91 80731 10841<br/>
+                        ✉️ likith1238@gmail.com
                   </div>
                    <div className='text1'>
-                    <h6 >Nayana</h6>
+                    <h6 >Nayana M</h6>
                     Member, IEEE SIT SB<br/>
-                    📞+91 9999999999<br/>
-                    ✉️ Nayana@gamil.com
+                    📞+91 63665 87209<br/>
+                    ✉️ nayana.273105@gmail.com
                   </div>
            </div>
              
