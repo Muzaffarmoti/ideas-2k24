db8a036fce06b8e39cc4edba8af87e8cfa2e4822
deekshas18 <deekshas182003@gmail.com>
2024-04-04 10:18:21 +0530

    pushing footer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Hero from './sections/Hero'
+import Footer from '../components/Footer'
 
 function Home() {
     return (
         <div>
             <Header />
             <Hero />
+            <Footer/>
         </div>
     )
 }
