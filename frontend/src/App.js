e85bf5a0eac07c9111b16b37e2be54b72f524807
Nayana0 <nayana.273105@gmail.com>
2024-04-05 01:59:26 +0530

    About us page updated
    updating about us

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import AboutUs from './pages/AboutUs';
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
     <div className="App">
       <AppRoutes/>
+      <AboutUs />
     </div>
   );
 }
