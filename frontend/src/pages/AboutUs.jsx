3965f3e3f8a12eac1a3921a1e46856382e90e4e7
Nayana0 <nayana.273105@gmail.com>
2024-04-07 01:37:32 +0530

    anim
    anim

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -20,12 +20,12 @@ function AboutUsComponents() {
             <div className="row section-header has-bottom-sep aos-init">
               <div className="col-full">
                 <h3 className="subhead"></h3>
-                <h1 className="display-2" style={{marginRight: spacing + 'em'}}>About S.I.T</h1>
+                <h1 className="display-2 display-2--light aos-init aos-animate" style={{marginRight: spacing + 'em'}}>About S.I.T</h1>
               </div>
             </div>
             <div className="row about-desc aos-init aos-animate">
               <div className="col-full" style={{color: '#f6f6f6'}}>
-                <p>Siddaganga Institute of Technology has a rich heritage of bestowing knowledge and passion to young minds.Its motto 'work is worship' is ingrained into the minds of every student, thus creating an inclusive and empowering community for students that encourages growth and achievement.The unwavering assistance from professors and administrators played a significant role in building the confidence and perspective of every individual.</p>
+                <p className="aos-init aos-animate">Siddaganga Institute of Technology has a rich heritage of bestowing knowledge and passion to young minds.Its motto 'work is worship' is ingrained into the minds of every student, thus creating an inclusive and empowering community for students that encourages growth and achievement.The unwavering assistance from professors and administrators played a significant role in building the confidence and perspective of every individual.</p>
               </div>
             </div>
           </section>
@@ -35,12 +35,12 @@ function AboutUsComponents() {
             <div className="row section-header has-bottom-sep aos-init aos-animate">
               <div className="col-full">
                 <h3 className="subhead subhead--dark"></h3>
-                <h1 className="display-1 display-1--light" style={{textTransform: 'uppercase'}}>About IEEE SIT SB</h1>
+                <h1 className="display-1 display-1--light aos-init aos-animate" style={{textTransform: 'uppercase'}}>About IEEE SIT SB</h1>
               </div>
             </div>
             <div className="row about-desc aos-init aos-animate">
               <div className="col-full">
-                <p>IEEE SIT SB was established in the year 2002 under the inspiring leadership of Professor Basavarajaiah. The legacy was followed by the vision and direction of our mentors Dr. K V Suresh, Professor, Department of ECE and Dr. N R Sunitha, Professor, Department of CSE. Our current branch counsellors Dr. K V Vishwanath, Professor, Department of ETE and Dr. Veena Karjagi, Associate Professor, Department of ECE IEEE SIT SB have won a lot of laurels. This is the only student branch from Bangalore Section to have been published in IEEE India Council Newsletter, 2019. IEEE SIT SB team is a force of diverse skills and expertise. The synergy of the team brings out the opportunity for every individual to explore, enhance and thereby strengthening the team. Providing insights and opportunities of IEEE, we fervently organize various events, technical colloquiums and contests. The WIE IEEE SIT SB, a wing under IEEE SIT SB supports all the innovation and technology of, for and by women through organizing various events.</p>
+                <p className="aos-init aos-animate">IEEE SIT SB was established in the year 2002 under the inspiring leadership of Professor Basavarajaiah. The legacy was followed by the vision and direction of our mentors Dr. K V Suresh, Professor, Department of ECE and Dr. N R Sunitha, Professor, Department of CSE. Our current branch counsellors Dr. K V Vishwanath, Professor, Department of ETE and Dr. Veena Karjagi, Associate Professor, Department of ECE IEEE SIT SB have won a lot of laurels. This is the only student branch from Bangalore Section to have been published in IEEE India Council Newsletter, 2019. IEEE SIT SB team is a force of diverse skills and expertise. The synergy of the team brings out the opportunity for every individual to explore, enhance and thereby strengthening the team. Providing insights and opportunities of IEEE, we fervently organize various events, technical colloquiums and contests. The WIE IEEE SIT SB, a wing under IEEE SIT SB supports all the innovation and technology of, for and by women through organizing various events.</p>
               </div>
             </div>
           </section>
