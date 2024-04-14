9d177934ebcccf85cfb9b6843b87f25d0cf3283c
Anagha-N13 <anagha2003.13@gmail.com>
2024-04-14 21:06:02 +0530

    conflicts made

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../Css/ContactUs.css'
 import art_video from '../assets/videos/Art_at_contact.mp4'
-import sinch from '../assets/images/web_team/sinchan.jpeg'
-
+import anagha from '../assets/images/web_team/anagha.jpg'
+import deeksha from '../assets/images/web_team/deeksha.jpg'
+import ananya from '../assets/images/web_team/ananya.jpg'
+import likith from '../assets/images/web_team/likith.jpg'
+import muzaffar from '../assets/images/web_team/muzaffar.jpeg'
+import nayana from '../assets/images/web_team/nayana.jpg'
+import rishith from '../assets/images/web_team/rishith.jpg'
+import ContactDetails from './ContactDetails'
 import Header from '../components/Header'
 
+
 function ContactUs() {
-       const memberDetailsArray = [
+    const memberDetailsArray = [
         {
             name: 'Anagha N',
             position: 'Lead, Web Master IEEE SIT SB',
@@ -112,20 +119,21 @@ function ContactUs() {
 
     return (
         <div className='contact_main_container'>
-            <Header/>
-            <h3 className='contact_header'>Contact us </h3>
+            <Header />
+            <div className='background-image'></div>
+            <h3 className='contact_header'>CONTACT US </h3>
             <div className='contact_container1' >
                 <div className="container_map" ><iframe width="600" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
                     src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Siddaganga%20Institute%20Of%20Technology%20Bangalore%20-%20Honnavar%20Hwy,%20Chandana%20Complex,%20Tumakuru,%20Karnataka%20572103+(SIT)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
                     <a href="https://www.gps.ie/">gps vehicle tracker</a></iframe>
                 </div>
                 <div className='cont'>
-                    <div className='back'>
+                    {/* <div className='back'>
                         <video autoPlay loop muted>
-                            <source src={art_video} type='video/mp4' />
+                            <source src={space} type='video/gif' />
                         </video>
-                    </div>
-                    <div className='content'>
+                    </div> */}
+                    {/* <div className='content'>
                         <div className='container_text1'>
                             <div className='text1'>
                                 <h6 >Kona Chandra Kiran</h6>
@@ -159,18 +167,18 @@ function ContactUs() {
                         <h5 className='contact_header clr'>HOW TO REACH US</h5>
                         <ul>
                             <li className='map_text_test'><span>BUS:</span>  Frequent buses are available to Tumakuru from the Kempegowda Bus Station,
-                             Bengaluru. Alight at the SIT stop one and a half hour later. If you happen to sleep
-                              through, fret not. Alight at the Tumakuru bus stand. Autos and the local buses (no. 201) are easily available to SIT </li>
+                                Bengaluru. Alight at the SIT stop one and a half hour later. If you happen to sleep
+                                through, fret not. Alight at the Tumakuru bus stand. Autos and the local buses (no. 201) are easily available to SIT </li>
                             <li className='map_text_test'><span>TRAIN:</span>  Trains are available intermittently from
-                             Bengaluru station. Kindly refer to the train schedules for timings. From Tumakuru station, autos are available to SIT. </li>
+                                Bengaluru station. Kindly refer to the train schedules for timings. From Tumakuru station, autos are available to SIT. </li>
                             <li className='map_text_test'><span>TAXI:</span>  Taxis are available from Bangalore to SIT. However, considering the frequency
-                             of buses,  taxis are not really recommended. </li>
+                                of buses,  taxis are not really recommended. </li>
                             <li className='map_text_test'><span>AIR:</span>  Air The nearest airport is the Kempegowda International Airport, Bengaluru. </li>
-                            </ul>
+                        </ul>
 
 
                     </div>
-           </div>
+                </div>
             </div>
             <div className='container_text1_test'>
                 <div className='text1'>
@@ -198,8 +206,8 @@ function ContactUs() {
                     ✉️ nandithabu53@gmail.com
                 </div>
             </div>
-            <h4 className='contact_header'>Web Team</h4>
-            <div className='web_container'>
+            {/* <h4 className='contact_header'>THE WEB TEAM</h4> */}
+            {/* <div className='web_container'>
                 <div className='container_text_sub1'>
                     <div className='text1'>
                         <h6 >Anagha N</h6>
@@ -208,7 +216,7 @@ function ContactUs() {
                         ✉️ anagha2003.13@gamil.com
                     </div>
                     <div className='text1'>
-                        <h6 >Rishith P</h6>
+                        <h6 >Rishith  P</h6>
                         Lead,Design Master IEEE SIT SB<br />
                         📞+91 78924 94498<br />
                         ✉️ rishithp2003@gmail.com
@@ -230,7 +238,7 @@ function ContactUs() {
                     <div className='text1'>
                         <h6 >Ananya BR</h6>
                         Member, IEEE SIT SB<br />
-                        📞+91 99110 449927 <br />
+                        📞+91 91104 49927 <br />
                         ✉️ ananyasetty127@gmail.com
                     </div>
                     <div className='text1'>
@@ -248,9 +256,9 @@ function ContactUs() {
                 </div>
 
             </div> */}
-                       
+
             {/* <h4 className='contact_header'>THE WEB TEAM</h4> */}
-         
+
             {/* <div className='pic'>
                 {hoveredImageDetails && (
                     <div className='image-details'>
@@ -263,135 +271,134 @@ function ContactUs() {
                             <div
                                 className='pic_sub2_img'
                                 key={index}
-                                onMouseEnter={() => handleMouseEnter(`${item.name}, ${item.position}`)}
+                                onMouseEnter={() => handleMouseEnter(${item.name}, ${item.position})}
                                 onMouseLeave={handleMouseLeave}
                             >
-                                <img className='img' src={item.image} alt={`Image ${index + 1}`} />
+                                <img className='img' src={item.image} alt={Image ${index + 1}} />
                             </div>
                         ))}
                     </div>
                     {/* Add more scrollers similarly */}
-                    {/* <div className='pic_sub2'>
+            {/* <div className='pic_sub2'>
                         {memberDetailsArray.map((item, index) => (
                             <div
                                 className='pic_sub2_img'
                                 key={index}
-                                onMouseEnter={() => handleMouseEnter(`${item.name}, ${item.position}`)}
+                                onMouseEnter={() => handleMouseEnter(${item.name}, ${item.position})}
                                 onMouseLeave={handleMouseLeave}
                             >
-                                <img className='img' src={item.image} alt={`Image ${index + 1}`} />
+                                <img className='img' src={item.image} alt={Image ${index + 1}} />
                             </div>
                         ))}
                     </div>
                 </div> } */}
-                {/* </div> */}
+            {/* </div> */}
 
-                {/* Render image details */}
-                
+            {/* Render image details */}
+
             {/* </div> */}
 
         </div>
-
     )
 }
 
 export default ContactUs
-            // <div className='pic'>
-            //     {/* <div className='pic_sub1'>
-            //         <div className='pic_sub2'>
-            //             <div id='image1' className='pic_sub2_img' data-description='Anagha WEB Master IEEE SIT SB'>
-            //                 <img className='img' src={anagha} alt='Anagha' />
-            //             </div>
-            //             <div id='image3' className='pic_sub2_img' data-description='3' >
-            //                 <img className='img' src={ananya} alt='Ananya' />
-            //             </div>
-            //             <div id='image2' className='pic_sub2_img'>
-            //                 <img className='img' src={deeksha} alt='Deeksha' data-description='2' />
-            //             </div>
-            //             <div id='image5' className='pic_sub2_img' data-description='5'>
-            //                 <img className='img' src={likith} alt='likith' />
-            //             </div>
-            //             <div id='image4' className='pic_sub2_img' data-description='4' >
-            //                 <img className='img' src={muzaffar} alt='muzaffar' />
-            //             </div>
+// <div className='pic'>
+//     {/* <div className='pic_sub1'>
+//         <div className='pic_sub2'>
+//             <div id='image1' className='pic_sub2_img' data-description='Anagha WEB Master IEEE SIT SB'>
+//                 <img className='img' src={anagha} alt='Anagha' />
+//             </div>
+//             <div id='image3' className='pic_sub2_img' data-description='3' >
+//                 <img className='img' src={ananya} alt='Ananya' />
+//             </div>
+//             <div id='image2' className='pic_sub2_img'>
+//                 <img className='img' src={deeksha} alt='Deeksha' data-description='2' />
+//             </div>
+//             <div id='image5' className='pic_sub2_img' data-description='5'>
+//                 <img className='img' src={likith} alt='likith' />
+//             </div>
+//             <div id='image4' className='pic_sub2_img' data-description='4' >
+//                 <img className='img' src={muzaffar} alt='muzaffar' />
+//             </div>
 
-            //             <div id='image6' className='pic_sub2_img' data-description='6' >
-            //                 <img className='img' src={nayana} alt='nayana' />
-            //             </div>
-            //             <div id='image7' className='pic_sub2_img' data-description='7'>
-            //                 <img className='img' src={rishith} alt='rishith ' />
-            //             </div>
+//             <div id='image6' className='pic_sub2_img' data-description='6' >
+//                 <img className='img' src={nayana} alt='nayana' />
+//             </div>
+//             <div id='image7' className='pic_sub2_img' data-description='7'>
+//                 <img className='img' src={rishith} alt='rishith ' />
+//             </div>
 
-            //         </div>
-            //         <div className='pic_sub2'>
-            //             <div className='pic_sub2_img' data-description='Anagha WEB Master IEEE SIT SB '>
-            //                 <img className='img' src={anagha} alt='Anagha' />
-            //             </div>
-            //             <div className='pic_sub2_img'>
-            //                 <img className='img' src={ananya} alt='Ananya' />
-            //             </div>
-            //             <div className='pic_sub2_img'>
-            //                 <img className='img' src={deeksha} alt='Deeksha' />
-            //             </div>
-            //             <div className='pic_sub2_img'>
-            //                 <img className='img' src={likith} alt='likith' />
-            //             </div>
-            //             <div className='pic_sub2_img'>
-            //                 <img className='img' src={muzaffar} alt='muzaffar' />
-            //             </div>
+//         </div>
+//         <div className='pic_sub2'>
+//             <div className='pic_sub2_img' data-description='Anagha WEB Master IEEE SIT SB '>
+//                 <img className='img' src={anagha} alt='Anagha' />
+//             </div>
+//             <div className='pic_sub2_img'>
+//                 <img className='img' src={ananya} alt='Ananya' />
+//             </div>
+//             <div className='pic_sub2_img'>
+//                 <img className='img' src={deeksha} alt='Deeksha' />
+//             </div>
+//             <div className='pic_sub2_img'>
+//                 <img className='img' src={likith} alt='likith' />
+//             </div>
+//             <div className='pic_sub2_img'>
+//                 <img className='img' src={muzaffar} alt='muzaffar' />
+//             </div>
 
-            //             <div className='pic_sub2_img'>
-            //                 <img className='img' src={nayana} alt='nayana' />
-            //             </div>
-            //             <div className='pic_sub2_img'>
-            //                 <img className='img' src={rishith} alt='rishith ' />
-            //             </div>
-            //         </div>
-            //     </div> */}
-            //     {/* <div className='pic'> */}
-            //     {/* Scroller elements */}
-            //     {/* {hoveredImageDetails && (
-            //         <div className='image-details'>
-            //             <p>{hoveredImageDetails}</p>
-            //         </div>
-            //     )}                 */}
-            //     <div className='image-details'>
-            //         {hoveredImageDetails ? (
-            //             <p>{hoveredImageDetails}</p>
-            //         ) : (
-            //             <p>Hover over an image to see details</p>
-            //         )}
-            //     </div>
-            //     <div className='pic_sub1'>
-            //         <div className='pic_sub2'>
-            //             {memberDetailsArray.map((item, index) => (
-            //                 <div
-            //                     className='pic_sub2_img'
-            //                     key={index}
-            //                     onMouseEnter={() => handleMouseEnter(`${item.name}, ${item.position}`)}
-            //                     onMouseLeave={handleMouseLeave}
-            //                 >
-            //                     <img className='img' src={item.image} alt={`Image ${index + 1}`} />
-            //                 </div>
-            //             ))}
-            //         </div>
-            //         {/* Add more scrollers similarly */}
-            //         <div className='pic_sub2'>
-            //             {memberDetailsArray.map((item, index) => (
-            //                 <div
-            //                     className='pic_sub2_img'
-            //                     key={index}
-            //                     onMouseEnter={() => handleMouseEnter(`${item.name}, ${item.position}`)}
-            //                     onMouseLeave={handleMouseLeave}
-            //                 >
-            //                     <img className='img' src={item.image} alt={`Image ${index + 1}`} />
-            //                 </div>
-            //             ))}
-            //         </div>
+//             <div className='pic_sub2_img'>
+//                 <img className='img' src={nayana} alt='nayana' />
+//             </div>
+//             <div className='pic_sub2_img'>
+//                 <img className='img' src={rishith} alt='rishith ' />
+//             </div>
+//         </div>
+//     </div> */}
+//     {/* <div className='pic'> */}
+//     {/* Scroller elements */}
+//     {/* {hoveredImageDetails && (
+//         <div className='image-details'>
+//             <p>{hoveredImageDetails}</p>
+//         </div>
+//     )}                 */}
+//     <div className='image-details'>
+//         {hoveredImageDetails ? (
+//             <p>{hoveredImageDetails}</p>
+//         ) : (
+//             <p>Hover over an image to see details</p>
+//         )}
+//     </div>
+//     <div className='pic_sub1'>
+//         <div className='pic_sub2'>
+//             {memberDetailsArray.map((item, index) => (
+//                 <div
+//                     className='pic_sub2_img'
+//                     key={index}
+//                     onMouseEnter={() => handleMouseEnter(${item.name}, ${item.position})}
+//                     onMouseLeave={handleMouseLeave}
+//                 >
+//                     <img className='img' src={item.image} alt={Image ${index + 1}} />
+//                 </div>
+//             ))}
+//         </div>
+//         {/* Add more scrollers similarly */}
+//         <div className='pic_sub2'>
+//             {memberDetailsArray.map((item, index) => (
+//                 <div
+//                     className='pic_sub2_img'
+//                     key={index}
+//                     onMouseEnter={() => handleMouseEnter(${item.name}, ${item.position})}
+//                     onMouseLeave={handleMouseLeave}
+//                 >
+//                     <img className='img' src={item.image} alt={Image ${index + 1}} />
+//                 </div>
+//             ))}
+//         </div>
 
-            //     </div>
-            //     {/* </div> */}
+//     </div>
+//     {/* </div> */}
 
-            //     {/* Render image details */}
+//     {/* Render image details */}
 
-            // </div>
+// </div>
