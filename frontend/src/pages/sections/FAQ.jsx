0156bb84bc7b2ae02f57c9127c3744628a259352
Ananya <ananyasetty127@gmail.com>
2024-04-16 23:13:03 +0530

    final commit

--- frontend/src/pages/sections/FAQ.jsx
+++ frontend/src/pages/sections/FAQ.jsx
@@ -8,57 +8,61 @@ const FAQ = () => {
         {
             id: 1,
             question: 'What is IdEEEas?',
-            answer:
-                'IdEEEas is a National level technical symposium organised by IEEE SIT-SB.',
+            answer: 'IdEEEas is a National level technical symposium organised by IEEE SIT-SB.',
             isOpen: false,
         },
         {
             id: 2,
             question: 'How many members can participate in a team?',
-            answer:
-                "The team size depends on the type of event.Do click on the event cards for more details.",
+            answer: 'The team size depends on the type of event. Do click on the event cards for more details.',
             isOpen: false,
         },
         {
             id: 3,
             question: 'How many sub-events are there in idEEEas?',
-            answer:
-                'There are 12 sub-events in IdEEEAs.',
+            answer: 'There are 12 sub-events in IdEEEAs.',
             isOpen: false,
         },
         {
             id: 4,
             question: 'If my abstract gets selected for two events Aavishkar and Technocon, can our team participate in both?',
-            answer:
-                'YES, You can participate.',
+            answer: 'YES.',
             isOpen: false,
         },
         {
             id: 5,
             question: 'If I am shortlisted for two rounds on the same day, will it be permissible to attend both?',
-            answer:
-                'YES, You are allowed.',
+            answer: 'YES',
             isOpen: false,
         },
         {
             id: 6,
             question: 'Where will I find the rules and regulations for the events?',
-            answer:
-                'Click on the event cards, download the brochure to know the rules and regulations.',
+            answer: 'Click on the event cards, download the brochure to know the rules and regulations.',
             isOpen: false,
         },
         {
             id: 7,
             question: 'For which events the on-spot registrations will be available?',
-            answer:
-                'Blitz, Cnario, IdEEEas idol, Photographia, Rectifier, Robotics, trIEEEvia and  Webify are the events in which on-spot regestrations are available.',
+            answer: 'Blitz, Cnario, IdEEEas idol, Photographia, Rectifier, Robotics, trIEEEvia and Webify are the events in which on-spot registrations are available.',
             isOpen: false,
         },
         {
             id: 8,
-            question: 'If I have any doubts , whom should I contact with?',
-            answer:
-                'Do refer the contact page.',
+            question: 'How do I register?',
+            answer: 'Do click the desired event card, this gives you the event details. click on the register button to register.One will get the ticket through mail.',
+            isOpen: false,
+        },
+        {
+            id: 9,
+            question: 'Can I rergister for multiple events?',
+            answer: 'Yes.You can register for multiple events.',
+            isOpen: false,
+        },
+        {
+            id: 10,
+            question: 'If I have any doubts, whom should I contact with?',
+            answer: 'Do refer the contact page',
             isOpen: false,
         },
     ]);
@@ -74,7 +78,7 @@ const FAQ = () => {
     return (
         <div className="faq-container">
             <div className='faq-heading-container'>
-            <h2 className='faq-heading'>FAQ</h2>
+                <h2 className='faq-heading'>FAQ</h2>
             </div>
             <div className='faq-mini-container'>
                 {faqs.map((faq) => (
@@ -84,7 +88,7 @@ const FAQ = () => {
                         onClick={() => toggleAnswer(faq.id)}
                     >
                         <img
-                            className="f-arrow-icon"
+                            className="arrow-icon"
                             src={faq.isOpen ? upArrow : downArrow}
                             alt={faq.isOpen ? 'Up Arrow' : 'Down Arrow'}
                         />
